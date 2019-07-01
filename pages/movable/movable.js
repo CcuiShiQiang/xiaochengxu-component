436f60140cf29994449a6a41e706bc58a9081617
CcuiShiQiang <946081940@qq.com>
2019-07-01 15:09:59 +0800

    初始化

---
+++ pages/movable/movable.js
@@ -0,0 +1,21 @@
+
+let config = {
+  data: {
+    x: 0,
+    y: 0
+  },
+  tap: function (e) {
+    this.setData({
+      x: 30,
+      y: 30
+    });
+  },
+  onChange: function (e) {
+    console.log(e.detail)
+  },
+  onScale: function (e) {
+    console.log(e.detail)
+  }
+};
+
+Page(config);
