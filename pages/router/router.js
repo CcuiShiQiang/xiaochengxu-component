5d1bf55c31f37d47a4a6d3329f1a1adbedb73bef
CcuiShiQiang <946081940@qq.com>
2019-07-12 09:28:34 +0800

    add router

---
+++ pages/router/router.js
@@ -0,0 +1,95 @@
+// pages/router/router.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  tologs: function () {
+    wx.switchTab({
+      url: '/pages/logs/logs'
+    })
+  },
+
+  tobasic: function () {
+    wx.reLaunch({
+      url: '/pages/basic/basic?id=1',
+    })
+  },
+
+  toinputPage: function () {
+    wx.redirectTo({
+      url: '/pages/inputComponent/inputPage/inputPage',
+    })
+  },
+
+  topickerPage: function () {
+    wx.navigateTo({
+      url: '/pages/inputComponent/pickerPage/pickerPage',
+    })
+  },
+
+  back: function () {
+    wx.navigateBack({
+      delta: 1
+    })
+  }
+})
